a04ab4daf8c235f84d69e1366b6d9be52ac6b05c
Anders Boyum <Acboyum@gmail.com>
2023-04-01 10:53:06 -0500

    Created a function to retrieve form submission field data and store as variables.
    Created function. Also event.preventDefault() on form element. Finally, cleared form submission fields.

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 $(document).ready(onReady);
 
 function onReady() {
-    console.log("We're Live!!")
+    $('#submitButton').on('click', submitInfo); // create input submission function to retrieve user data
 
 }
 
+
+
+// Do I need to worry about non-number + non-string input, and rejecting it? ---STRETCH GOAL?---
+function submitInfo(event) {
+    event.preventDefault();
+
+    // retrieve employee data, store as variables
+    let firstNameInput = $('#firstName').val();
+    let lastNameInput = $('#lastName').val();
+    let idInput = Number($('#id').val());
+    let titleInput = $('#title').val();
+    let salaryInput = Number($('#salary').val());
+
+    // clear form submission fields
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#id').val('');
+    $('#title').val('');
+    $('#salary').val('');
+
+}
